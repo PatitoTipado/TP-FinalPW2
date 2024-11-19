6c2a4462b1d510cbffce129ea2aca89713269540
NaN <mauriciodominguez9876543@gmail.com>
2024-11-19 17:29:25 -0300

    Listar en admins listo
    Que pueda obtener su pais y ciudad desde su localizacion listo

--- controller/AdminController.php
+++ controller/AdminController.php
@@ -15,33 +15,13 @@ class AdminController
     {
         $this->validarAdministrador();
 
-        $this->presenter->show('admin',$_SESSION);
-    }
-
-    public function listarUsuarios()
-    {
-        $this->validarAdministrador();
-
         $this->presenter->show('listarUsuario',$_SESSION);
     }
-
-//    public function jugadoresTotales()
-//    {
-//
-//        $this->validarAdministrador();
-//        $_SESSION['jugadores_totales']=true;
-//
-//        $data = $this->model->obtenerJugadoresTotales();
-//
-//        $dataCompleto= array_merge($_SESSION,$data);
-//
-//        $this->presenter->show('listarUsuario',$dataCompleto);
-//        unset($_SESSION['jugadores_totales']);
-//    }
 
     public function jugadoresTotales()
     {
         $this->validarAdministrador();
+
         $_SESSION['jugadores_totales'] = true;
 
         // Obtener los jugadores agrupados por estado
@@ -64,6 +44,145 @@ class AdminController
         unset($_SESSION['jugadores_totales']);
     }
 
+    public function usuariosNuevos()
+    {
+        $this->validarAdministrador();
+        $_SESSION['jugadores_nuevos'] = true;
+
+        $fechaInicio= $_GET['fecha_inicio']?? '';
+        $fechaFin= $_GET['fecha_fin'] ?? '';
+
+        $dataCompleto=$_SESSION;
+
+        if(!empty($fechaFin) && !empty($fechaInicio)){
+            $data= $this->model->filtrarUsuariosPorRangoDeFecha($fechaInicio,$fechaFin);
+            $data['cantidad_total']= $this->model->obtenerLaCantidadDeJugadoresTotales();
+            $dataCompleto = array_merge($_SESSION, $data);
+        }
+
+        $this->presenter->show('listarUsuario', $dataCompleto);
+
+        unset($_SESSION['jugadores_nuevos']);
+    }
+
+    public function filtroPais()
+    {
+        $this->validarAdministrador();
+        $_SESSION['filtro_pais'] = true;
+
+        $pais= $_GET['pais']?? '';
+        $ciudad=$_GET['ciudad'] ??'';
+        $dataCompleto=$_SESSION;
+
+        if(!empty($pais) && !empty($ciudad)){
+            $data= $this->model->filtrarPorPaisYCiudad($pais,$ciudad);
+            $data['cantidad_total']= $this->model->obtenerLaCantidadDeJugadoresTotales();
+            $dataCompleto = array_merge($_SESSION, $data);
+        }
+
+        $this->presenter->show('listarUsuario', $dataCompleto);
+
+        unset($_SESSION['filtro_pais']);
+
+    }
+
+    public function filtroSexo()
+    {
+        $this->validarAdministrador();
+
+        $_SESSION['filtro_sexo'] = true;
+
+        // Obtener los jugadores agrupados por sexo
+        $jugadoresPorSexo = $this->model->filtrarPorSexo();
+
+        // Verifica si hay resultados antes de pasarlos a la vista
+        if (!$jugadoresPorSexo) {
+            die("No se encontraron jugadores.");
+        }
+
+        // Pasar los datos a la vista
+        $data = ['jugadoresPorSexo' => $jugadoresPorSexo];
+
+        // Combina datos con la sesión si es necesario
+        $dataCompleto = array_merge($_SESSION, $data);
+
+        // Pasar los datos a la vista
+        $this->presenter->show('listarUsuario', $dataCompleto);
+
+        unset($_SESSION['filtro_sexo']);
+    }
+
+    public function filtroEdad()
+    {
+        $this->validarAdministrador();
+
+        $_SESSION['filtro_edad'] = true;
+
+        // Obtener los jugadores agrupados por sexo
+        $jugadoresPorEdad = $this->model->filtrarPorEdad();
+
+        // Verifica si hay resultados antes de pasarlos a la vista
+        if (!$jugadoresPorEdad) {
+            die("No se encontraron jugadores.");
+        }
+
+        // Pasar los datos a la vista
+        $data = ['jugadoresPorEdad' => $jugadoresPorEdad];
+
+        // Combina datos con la sesión si es necesario
+        $dataCompleto = array_merge($_SESSION, $data);
+
+        // Pasar los datos a la vista
+        $this->presenter->show('listarUsuario', $dataCompleto);
+
+        unset($_SESSION['filtro_edad']);
+
+    }
+
+    public function listarTotalPartidas()
+    {
+        $this->validarAdministrador();
+
+        $_SESSION['partidas_totales'] = true;
+
+        $partidasPorEstado = $this->model->obtenerPartidasPorEstado();
+
+        if (!$partidasPorEstado) {
+            die("No se encontraron partidas.");
+        }
+
+        $data = ['partidasPorEstado' => $partidasPorEstado];
+
+        $dataCompleto = array_merge($_SESSION, $data);
+
+        $this->presenter->show('listarPartidas', $dataCompleto);
+
+        unset($_SESSION['partidas_totales']);
+    }
+
+    public function listarPartidasPorFecha()
+    {
+        $this->validarAdministrador();
+
+        $inicio=$_GET['fecha_inicio'];
+        $fin= $_GET['fecha_fin'];
+
+        $_SESSION['partidas_totales'] = true;
+
+        $partidasPorEstado = $this->model->obtenerPartidasPorEstadoFiltradasPorFecha($inicio,$fin);
+
+        if (!$partidasPorEstado) {
+            die("No se encontraron partidas.");
+        }
+
+        $data = ['partidasPorEstado' => $partidasPorEstado];
+
+        $dataCompleto = array_merge($_SESSION, $data);
+
+        $this->presenter->show('listarPartidas', $dataCompleto);
+
+        unset($_SESSION['partidas_totales']);
+    }
 
 
     public function validarAdministrador()
--- model/AdminModel.php
+++ model/AdminModel.php
@@ -34,4 +34,191 @@ class AdminModel
         return $data; // Retornar los resultados como un arreglo de filas
     }
 
+    public function filtrarUsuariosPorRangoDeFecha($inicio, $fin)
+    {
+        if ($inicio <= $fin) {
+            $sql = "SELECT COUNT(*) AS cantidad FROM usuarios WHERE fecha_registro BETWEEN '$inicio' AND '$fin' AND rol = 'jugador'";
+            $data['result'] = false;
+
+            $result = $this->database->query($sql);
+
+            if ($result) {
+                $data['result']=true;
+                $data['cantidad'] = $result[0]['cantidad'] ?? 0;
+            } else {
+                $data['error'] = "Error al ejecutar la consulta.";
+            }
+
+            return $data;
+        }
+
+        $data['error'] = "La fecha de inicio no puede ser mayor que la fecha de fin.";
+        return $data;
+    }
+
+    public function obtenerLaCantidadDeJugadoresTotales()
+    {
+        $sql = "SELECT COUNT(*) AS cantidad FROM usuarios WHERE rol = 'jugador'";
+        $data['result'] = false;
+
+        $result = $this->database->query($sql);
+
+        if ($result) {
+            return $result[0]['cantidad'] ?? 0;
+        }
+
+        return $data;
+    }
+
+    public function filtrarPorPaisYCiudad($pais, $ciudad)
+    {
+        $sql = "SELECT COUNT(*) AS cantidad FROM usuarios WHERE pais = '$pais' AND ciudad= '$ciudad' AND rol = 'jugador'";
+        $data['result'] = false;
+
+        $result = $this->database->query($sql);
+        $data['cantidad'] = "no se encontraron resultados";
+
+        if ($result) {
+            $data['result']=true;
+            $data['cantidad'] = $result[0]['cantidad'];
+        }
+
+        return $data;
+    }
+
+    public function filtrarPorSexo()
+    {
+        $query = "SELECT sexo AS sexo, COUNT(*) AS cantidad FROM usuarios WHERE rol = 'jugador' GROUP BY sexo";
+
+        $result = mysqli_query($this->database->getConn(), $query);
+
+        if (!$result) {
+            die("Error en la consulta SQL: " . mysqli_error($this->database->getConn()));
+        }
+
+        $data = [];
+
+        while ($row = mysqli_fetch_assoc($result)) {
+            $data[] = $row; // Añadir cada fila a $data
+        }
+
+        return $data;
+
+    }
+
+    public function filtrarPorEdad()
+    {
+        $fechaActual= $this->database->obtenerFechaActual();
+
+        $data['menores']= $this->usuariosMenoresDe18($fechaActual);
+
+        $data['adultos']=$this->usuariosEntre18y60($fechaActual);
+
+        $data['jubilados']= $this->usuariosMayoresDe60($fechaActual);
+
+        return $data;
+    }
+
+    public function usuariosMenoresDe18($fechaActual)
+    {
+        $sql = "SELECT COUNT(*) AS cantidad 
+        FROM usuarios 
+        WHERE rol = 'jugador' 
+        AND YEAR(CURDATE()) - anio_de_nacimiento < 18";
+
+        $result = $this->database->query($sql);
+
+        return $result[0]['cantidad'] ?? 0;
+    }
+
+    public function usuariosEntre18y60($fechaActual)
+    {
+        $sql = "SELECT COUNT(*) AS cantidad 
+        FROM usuarios 
+        WHERE rol = 'jugador' 
+        AND YEAR(CURDATE()) - anio_de_nacimiento BETWEEN 18 AND 59";
+
+        $result = $this->database->query($sql);
+
+        return $result[0]['cantidad'] ?? 0;
+    }
+
+    public function usuariosMayoresDe60($fechaActual)
+    {
+        $sql = "SELECT COUNT(*) AS cantidad 
+        FROM usuarios 
+        WHERE rol = 'jugador' 
+        AND YEAR(CURDATE()) - anio_de_nacimiento >= 60";
+
+        $result = $this->database->query($sql);
+
+        return $result[0]['cantidad'] ?? 0;
+    }
+
+    public function obtenerPartidasPorEstado()
+    {
+        $query = "SELECT estado, COUNT(*) AS cantidad FROM partidas GROUP BY estado";
+
+        $result = mysqli_query($this->database->getConn(), $query);
+
+        if (!$result) {
+            die("Error en la consulta SQL: " . mysqli_error($this->database->getConn()));
+        }
+
+        $data = [];
+
+        while ($row = mysqli_fetch_assoc($result)) {
+            $data[] = $row;
+        }
+
+        return $data;
+
+    }
+
+    public function obtenerPartidasPorEstadoFiltradasPorFecha($inicio, $fin)
+    {
+        // Si ambos campos están vacíos, devolver todas las partidas agrupadas por estado
+        if (empty($inicio) && empty($fin)) {
+            return $this->obtenerPartidasPorEstado();
+        }
+
+        // Construir las condiciones del WHERE dinámicamente
+        $conditions = [];
+
+        if (!empty($inicio)) {
+            $conditions[] = "fecha_de_partida >= '$inicio'";
+        }
+
+        if (!empty($fin)) {
+            $conditions[] = "fecha_de_finalizacion <= '$fin'";
+        }
+
+        // Unir las condiciones con "AND"
+        $whereClause = implode(' AND ', $conditions);
+
+        // Crear la consulta completa
+        $query = "SELECT estado, COUNT(*) AS cantidad FROM partidas";
+        if (!empty($whereClause)) {
+            $query .= " WHERE $whereClause";
+        }
+        $query .= " GROUP BY estado";
+
+        // Ejecutar la consulta
+        $result = mysqli_query($this->database->getConn(), $query);
+
+        if (!$result) {
+            throw new Exception("Error en la consulta SQL: " . mysqli_error($this->database->getConn()));
+        }
+
+        // Transformar los resultados en un array
+        $data = [];
+        while ($row = mysqli_fetch_assoc($result)) {
+            $data[] = $row;
+        }
+
+        return $data;
+    }
+
+
+
 }
