78b7fb6c7937bb2da04704d2faf4b9c1a85bf3a7
NaN <mauriciodominguez9876543@gmail.com>
2025-01-28 21:55:44 -0300

    refactor editor para que cumpla el patron de diseño mvc

--- configuration/Configuration.php
+++ configuration/Configuration.php
@@ -20,12 +20,6 @@ include_once("controller/PerfilController.php");
 include_once("controller/PartidaController.php");
 include_once ("controller/RankingController.php");
 include_once ("controller/EditorController.php");
-include_once ("controller/VerPreguntasController.php");
-include_once ("controller/ModificarPreguntaController.php");
-include_once ("controller/AgregarPreguntaController.php");
-include_once ("controller/EliminarPreguntaController.php");
-include_once ("controller/VerSugeridasController.php");
-include_once ("controller/VerReportesController.php");
 include_once ("controller/AdminController.php");
 include_once ("controller/ReporteController.php");
 
@@ -73,36 +67,6 @@ class Configuration
         return new EditorController($this->getPresenter(), $this->getPreguntaModel());
     }
 
-    public function getVerPreguntasController()
-    {
-        return new VerPreguntasController($this->getPresenter(), $this->getPreguntaModel());
-    }
-
-    public function getModificarPreguntaController()
-    {
-        return new ModificarPreguntaController($this->getPresenter(), $this->getPreguntaModel());
-    }
-
-    public function getEliminarPreguntaController()
-    {
-        return new EliminarPreguntaController($this->getPresenter(), $this->getPreguntaModel());
-    }
-
-    public function getAgregarPreguntaController()
-    {
-        return new AgregarPreguntaController($this->getPresenter(), $this->getPreguntaModel());
-    }
-
-    public function getVerSugeridasController()
-    {
-        return new VerSugeridasController($this->getPresenter(), $this->getPreguntaModel());
-    }
-
-    public function getVerReportesController()
-    {
-        return new VerReportesController($this->getPresenter(), $this->getPreguntaModel());
-    }
-  
     public function getReporteController()
     {
         return new ReporteController($this->getPresenter(),$this->getPartidaModel());
--- controller/EditorController.php
+++ controller/EditorController.php
@@ -18,6 +18,142 @@ class EditorController
         $this->presenter->show('editor');
     }
 
+    public function agregar()
+    {
+        $this->validarEditor();
+
+        $pregunta= isset($_POST["pregunta"]) ?? '';
+        $opcion1= isset($_POST["opcion1"]) ??'';
+        $opcion2= isset($_POST["opcion2"]) ??'';
+        $opcion3= isset($_POST["opcion3"]) ??'';
+        $opcionCorrecta= isset($_POST["opcionCorrecta"]) ??'';
+        $nivel= isset($_POST["nivel"]) ??'';
+
+
+        if(empty($pregunta) || empty($opcionCorrecta) || empty($opcion1) || empty($opcion2) || empty($opcion3) || empty($nivel)){
+            $this->presenter->show('agregarPregunta');
+        }
+
+        if (isset($_POST["pregunta"]) && isset($_POST["opcion1"]) && isset($_POST["opcion2"]) && isset($_POST["opcion3"]) && isset($_POST["opcionCorrecta"]) && isset($_POST['nivel'])) {
+            $pregunta = $_POST['pregunta'];
+            $opcion1 = $_POST['opcion1'];
+            $opcion2 = $_POST['opcion2'];
+            $opcion3 = $_POST['opcion3'];
+            $opcionCorrecta = $_POST['opcionCorrecta'];
+            $nivel = $_POST['nivel'];
+
+            $result = $this->model->agregarPreguntaConOpciones($pregunta, $nivel, $opcion1, $opcion2, $opcion3, $opcionCorrecta);
+
+            if (is_string($result)) {
+                $_SESSION["mensaje"] = "hubo un error al agregar o ya existe o no completo todos los campos";
+            } else {
+                $_SESSION["mensaje"] = "la pregunta se agrego correctamente";
+            }
+            header("Location: /editor/agregar");
+            exit();
+        }
+    }
+
+    public function mostrarPreguntas()
+    {
+        $this->validarEditor();
+        $data['preguntas'] = $this->model->obtenerPreguntas();
+        $this->presenter->show('verPreguntas', $data);
+    }
+
+    public function verPregunta()
+    {
+        $id = $_GET['id']?? '';
+
+        $this->validarEditor();
+
+        if(empty($id)){
+            header("Location: /editor");
+            exit();
+        }
+
+        $data['pregunta'] = $this->model->obtenerPregunta($id);
+        $this->presenter->show('pregunta', $data);
+    }
+
+    public function modificar()
+    {
+        $id = $_GET['id'];
+
+        $pregunta = isset($_POST['pregunta']) ? $_POST['pregunta'] : "";
+        $opcion1 = isset($_POST['opcion1']) ? $_POST['opcion1'] : "";
+        $opcion2 = isset($_POST['opcion2']) ? $_POST['opcion2'] : "";
+        $opcion3 = isset($_POST['opcion3']) ? $_POST['opcion3'] : "";
+        $opcionCorrecta = isset($_POST['opcionCorrecta']) ? $_POST['opcionCorrecta'] : "";
+        $nivel = $_POST['nivel'];
+
+        $this->model->modificarPreguntaConOpciones($id, $pregunta, $opcion1, $opcion2, $opcion3, $opcionCorrecta, $nivel);
+
+        header("location:/editor/mostrarPreguntas");
+        exit();
+    }
+
+    public function eliminar()
+    {
+        $this->validarEditor();
+
+        $id = $_GET['id'];
+        $this->model->eliminarPreguntaConOpciones($id);
+        header("location:/editor/mostrarPreguntas");
+        exit();
+    }
+
+    public function verSugeridas()
+    {
+        $this->validarEditor();
+        $data['sugeridas'] = $this->model->obtenerPreguntasSugeridas();
+        $this->presenter->show('verSugeridas', $data);
+    }
+
+    public function aprobarSugerida() {
+        $this->validarEditor();
+
+        $id = $_GET['id']??'';
+
+        $this->model->aprobarPreguntaSugerida($id);
+        header("location:/editor/mostrarPreguntas");
+        exit();
+    }
+
+    public function rechazarSugerida() {
+        $this->validarEditor();
+
+        $id = $_GET['id']??'';
+
+        $this->model->rechazarPreguntaSugerida($id);
+        header("location:/editor/verSugeridas");
+        exit();
+    }
+
+    public function verReportes()
+    {
+        $this->validarEditor();
+        $data['reportes'] = $this->model->obtenerPreguntasReportadas();
+        $this->presenter->show('verReportes', $data);
+    }
+
+    public function aprobarReporte() {
+        $id = $_GET['id']??'';
+
+        $this->model->aprobarPreguntaReportada($id);
+        header("location:/verPreguntas");
+        exit();
+    }
+
+    public function eliminarReporte() {
+        $id = $_GET['id']??'';
+
+        $this->model->eliminarPreguntaReportada($id);
+        header("location:/verSugeridas");
+        exit();
+    }
+
+
     public function validarEditor()
     {
         if (!isset($_SESSION['user'])) {
